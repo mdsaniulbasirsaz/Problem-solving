175cb1fc94cb7f467b565722e2ea541f833bee8b
Md Saniul Basir Saz <96694508+mdsaniulbasirsaz@users.noreply.github.com>
2023-03-22 11:37:11 +0600

    2023

---
+++ C_Crane_and_Turtle.cpp
@@ -0,0 +1,56 @@
+/* Md Saniul Basir Saz 
+   Department Of Computer Science and Engineering 
+   Jashore University Of Scinece and Technology 
+   Student Id: 200103 
+   Email: saniul.cse.just@gmail.com
+   Mobile: 01306032236 */
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<queue>
+#include<bits/stdc++.h>
+#include<string>
+#define The_End return 0
+#define nl '\n'
+#define YSSS cout<<"YES"<<nl
+#define NOOO cout<<"NO"<<nl
+#define ysss cout<<"Yes"<<<nl
+#define nooo cout<<"No"<<nl
+#define ll long long 
+#define fli(i,a,n) for(int i=a;i<n;i++)
+#define flj(j,a,n) for(int j=a;j<n;j++)
+#define flri(i,a,n) for(int i=n;i>=a;i--)
+#define flrj(j,a,n) for(int j=n;j>=a;j--)
+#define mod 1000000007
+#define st(n) fixed<<setprecision(n)
+#define my_code ios_base::sync_with_stdio(0);cout.tie(0);
+using namespace std;
+int main()
+{
+  my_code
+   int X, Y;
+   
+    cin >> X >> Y;
+
+    bool found = false;
+    for (int c = 0; c <= X; c++) {
+        int t = X - c;
+        int legs = 2 * c + 4 * t;
+        int animals = c + t;
+        if (legs == Y && animals == X) {
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        cout << "No" << endl;
+    }
+    else
+    {
+        cout<<"Yes"<<nl;
+    }
+  The_End;
+}
